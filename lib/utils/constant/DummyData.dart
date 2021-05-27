651502811d4768d8298cc9357077773898ec48f9
Deepak Sharma <deepaksharma040695@gmail.com>
2021-05-27 19:38:56 +0530

    Home page

--- lib/utils/constant/DummyData.dart
+++ lib/utils/constant/DummyData.dart
@@ -19,24 +19,23 @@ CategoryBean('Cars', '', 'https://stimg.cardekho.com/images/carexteriorimages/36
 CategoryBean('Street Art', '', 'https://wallpaperaccess.com/full/210623.jpg'),
 CategoryBean('Dark', '', 'https://cdn.statically.io/img/www.wallpaperwolf.com/wallpapers/4k-wallpapers/4k/download/dark-forest-0131-4k.png'),
 CategoryBean('Random', '', 'https://cdn.wallpapersafari.com/17/16/1lefPD.jpg'),
-// list.add(CategoryBean('', '', ''));
+// CategoryBean('', '', ''),
 ];
 
 
-colorCategoryBean() {
-  List<CategoryBean> list = [];
-  list.add(CategoryBean('Black', '#000000',
-      'https://c4.wallpaperflare.com/wallpaper/636/353/280/spider-man-wallpaper-preview.jpg'));
-  list.add(CategoryBean('White', '#FFFFFF',
-      'https://bestwallpapers.in/wp-content/uploads/2018/04/cats-kitten-cute-white-brown-5k-wallpaper-2560x1600.jpg'));
-  list.add(CategoryBean('Red', '#FF0000',
-      'https://images.hdqwalls.com/wallpapers/red-mask-neon-eyes-4k-0k.jpg'));
-  list.add(CategoryBean('Green', '#00FF00',
-      'https://cdn.statically.io/img/wallpaperaccess.com/full/545792.jpg'));
-  list.add(CategoryBean('Blue', '#0000FF',
-      'https://www.printawallpaper.com/wp-content/uploads/2020/07/Blue_Guitar_on_Brick_Wall_dn.jpg'));
-  list.add(CategoryBean(
-      'Yellow', '#FFFF00', 'https://wallpapercave.com/wp/wp8106092.jpg'));
-  list.add(CategoryBean('Violet', '#EE82EE',
-      'https://images.alphacoders.com/110/thumb-1920-1103203.png'));
-}
+  List<CategoryBean>  colorCategoryBean() =>[ 
+  CategoryBean('Black', '#000000',
+      'https://c4.wallpaperflare.com/wallpaper/636/353/280/spider-man-wallpaper-preview.jpg'),
+  CategoryBean('White', '#FFFFFF',
+      'https://bestwallpapers.in/wp-content/uploads/2018/04/cats-kitten-cute-white-brown-5k-wallpaper-2560x1600.jpg'),
+  CategoryBean('Red', '#FF0000',
+      'https://images.hdqwalls.com/wallpapers/red-mask-neon-eyes-4k-0k.jpg'),
+  CategoryBean('Green', '#00FF00',
+      'https://cdn.statically.io/img/wallpaperaccess.com/full/545792.jpg'),
+  CategoryBean('Blue', '#0000FF',
+      'https://www.printawallpaper.com/wp-content/uploads/2020/07/Blue_Guitar_on_Brick_Wall_dn.jpg'),
+  CategoryBean(
+      'Yellow', '#FFFF00', 'https://wallpapercave.com/wp/wp8106092.jpg'),
+  CategoryBean('Violet', '#EE82EE',
+      'https://images.alphacoders.com/110/thumb-1920-1103203.png'),
+];
