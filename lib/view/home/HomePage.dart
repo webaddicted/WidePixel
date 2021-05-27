fc0aad6c05f3f712b20982a9c955b339a8cb04ce
Deepak Sharma <deepaksharma040695@gmail.com>
2021-05-27 09:52:20 +0530

    Home Screen

--- lib/view/home/HomePage.dart
+++ lib/view/home/HomePage.dart
@@ -1,35 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wallpaper/data/controller/HomeController.dart';
+import 'package:wallpaper/utils/common/WidgetHelper.dart';
 import 'package:wallpaper/utils/constant/ColorConst.dart';
+import 'package:wallpaper/utils/constant/StrConst.dart';
 
-class HomePage extends GetView<HomeController>{
-
+class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: ColorConst.WHITE_COLOR,
-      body: Center(
-        child: SingleChildScrollView(
-          physics: BouncingScrollPhysics(),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(height: 180),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  margin: EdgeInsets.only(bottom: 70),
-                  child: CircularProgressIndicator(
-                    valueColor:
-                        new AlwaysStoppedAnimation<Color>(ColorConst.APP_COLOR),
-                  ),
-                ),
-              )
-            ],
+        backgroundColor: ColorConst.WHITE_COLOR,
+        body: SafeArea(child: createUi()));
+  }
+
+  Widget createUi() {
+    return CustomScrollView(
+      slivers: <Widget>[
+        toobar(),
+        // _contentSection(data),
+      ],
+    );
+  }
+
+  Widget toobar() {
+    return SliverAppBar(
+      leading: Container(
+        child: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
           ),
+          onPressed: () {
+          },
+        ),
+      ),
+      elevation: 0,
+      backgroundColor: Colors.transparent,
+      // expandedHeight: Get.height - 50,
+      snap: false,
+      pinned: false,
+      flexibleSpace: FlexibleSpaceBar(
+        background: Container(
+          decoration: BoxDecoration(color: Colors.transparent),
         ),
       ),
     );
+    // return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+    //   IconButton(
+    //       icon: Icon(
+    //         Icons.sort, //menu,//dehaze,
+    //         color: ColorConst.BLACK_COLOR,
+    //       ),
+    //       onPressed: () {
+    //         // _scaffoldKey.currentState.openDrawer();
+    //       }),
+    //   getTxtAppColor(
+    //       msg: StrConst.APP_NAME, fontSize: 18, fontWeight: FontWeight.bold),
+    // ]);
   }
 }
