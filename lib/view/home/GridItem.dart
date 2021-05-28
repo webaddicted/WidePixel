0dc6df65460c82a1edf8d5ee0770328bd9a158ec
Deepak Sharma <deepaksharma040695@gmail.com>
2021-05-28 19:40:13 +0530

    List Pic

--- lib/view/home/GridItem.dart
+++ lib/view/home/GridItem.dart
@@ -2,7 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:wallpaper/utils/common/WidgetHelper.dart';
+import 'package:wallpaper/utils/constant/ColorConst.dart';
 import 'package:wallpaper/utils/constant/DummyData.dart';
+import 'package:wallpaper/utils/constant/RoutersConst.dart';
 
 /// Author : Deepak Sharma(Webaddicted)
 /// Email : deepaksharmatheboss@gmail.com
@@ -19,15 +21,6 @@ class GridItem extends StatelessWidget {
   Widget build(BuildContext context) {
     double height = 350;
     double width = Get.width - 15;
-    // if(heigthWidth == ApiConstant.Circle_100){
-    //    height = 100;
-    //    width = 100;
-    //    data = categoryBean();
-    // }else if(heigthWidth == ApiConstant.Circle_Color_50){
-    //   height = 50;
-    //   width = 50;
-    //   data = colorCategoryBean();
-    // }
     return Column(
       children: [
         getHeading(title: heigthWidth, onClick: (String title) {}),
@@ -38,39 +31,17 @@ class GridItem extends StatelessWidget {
             widget: (context, index) {
               var item = data[index];
 
-              return Container(
-                  margin: EdgeInsets.all(2),
-                  height: 50,
-                  child: getCacheImage(url: item.url, height: 50));
+              return InkWell(
+                onTap: ()=>Get.toNamed(RoutersConst.list),
+                splashColor: ColorConst.SPLASH_COLOR,
+                child: Container(
+                    margin: EdgeInsets.all(2),
+                    height: 50,
+                    child: getCacheImage(url: item.url, height: 50)),
+              );
             }),
       ],
     );
   }
 
-  Widget getView(double height, double width) {
-    return Container(
-      height: height,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      child: Row(
-        children: [
-          getCacheImage(url: data[5].url, height: height, width: width / 2),
-          SizedBox(width: 3),
-          Column(
-            children: [
-              getCacheImage(
-                  url: data[1].url, height: (height / 2) - 2, width: width / 2),
-              SizedBox(height: 3),
-              getCacheImage(
-                  url: data[3].url, height: (height / 2) - 2, width: width / 2),
-            ],
-          ),
-        ],
-      ),
-      // Positioned.fill(
-      //     child: Material(
-      //         color: Colors.transparent,
-      //         child: InkWell(
-      //             splashColor: ColorConst.SPLASH_COLOR, onTap: () {}))),
-    );
-  }
 }
