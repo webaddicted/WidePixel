32db5b14d00f4bfb73cb1977532a0cb4ed4965d8
Deepak Sharma <deepaksharma040695@gmail.com>
2021-06-08 08:08:54 +0530

    Pagination in list

--- lib/utils/constant/ApiConstant.dart
+++ lib/utils/constant/ApiConstant.dart
@@ -1,17 +1,19 @@
 class ApiConstant {
-  static final String BASE_URL = 'https://ezeerides.piousindia.com/';
-  static final String API_KEY = "78b9f63937763a206bff26c070b94158";
+  static final String BASE_URL = 'https://api.unsplash.com/';
+  static final String UnsplashApiKey =
+      "e993cde7a4d49aa482dd572dfca4dd27891fc573c4f5bed7f202e156e02b8e8e";
   static final String NoInternetConnection = "No Internet Connection";
   static String DEMO_IMG =
       'https://avatars0.githubusercontent.com/u/38448422?s=460&u=21b610183d275611a9bc0f730653d931b39f2d0b&v=4';
-  static final int TIME_OUT = 1500000;
+  static final int TIMEOUT = 1500000;
+  static const PAGE_SIZE = 20;
+
   static String known = 'Known';
   static String WHATSAPP_URL =
       'https://api.whatsapp.com/send?phone=+919024061407';
   static var LANGUAGE = 'en-US';
-  static var ABOUT_US = 'api/cancel-booking';
-
-  static var ACCESS_TOKEN = '';
+  static var PHOTOS = 'photos';
+  static var SEARCH_PHOTOS = 'search/photos';
 
   static var Rect_220_155 = 'Rect_220_155';
   static var Rect_320_15 = 'Rect_320_15';
@@ -20,6 +22,4 @@ class ApiConstant {
 
   static String Circle_100 = 'Circle 100';
   static String Circle_Color_50 = 'Circle color 50';
-
-
 }
