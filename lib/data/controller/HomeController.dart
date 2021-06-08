ef330ebd0d0a14d3243286094d86469dcf9b2ab7
Deepak Sharma <deepaksharma040695@gmail.com>
2021-06-08 08:10:36 +0530

    Pagination in list

--- lib/data/controller/HomeController.dart
+++ lib/data/controller/HomeController.dart
@@ -1,20 +1,34 @@
 import 'package:get/get.dart';
+import 'package:wallpaper/data/bean/PhotoOrderRespo.dart';
+import 'package:wallpaper/data/bean/SearchPhotoRespo.dart';
 import 'package:wallpaper/data/repo/HomeRepo.dart';
-// import 'package:wallpaper/utils/apiutils/api_response.dart';
-import 'package:wallpaper/utils/sp/sp_manager.dart';
+import 'package:wallpaper/utils/api_response.dart';
+import 'package:wallpaper/utils/constant/ApiConstant.dart';
 
 class HomeController extends GetxController {
   static HomeController get to => Get.find();
   HomeRepo _homeRepo;
-  HomeController(this._homeRepo);
-  // final allCityRespo = (ApiResponse<UpdateProfileReq>()).obs;
 
-// allCity() async {
-//   allCityRespo.value = ApiResponse.loading();
-//   if (employeeId.isEmpty) employeeId = await getCustomerId();
-//   final data = await _homeRepo.allCity(employeeId);
-//   allCityRespo.value = data;
-//   // print("LoginRespo : ${callLoginRespo}");
-//   return data;
-// }
+  HomeController(this._homeRepo);
+
+  final photoOrderRespo = (ApiResponse<List<PhotoOrderRespo>?>()).obs;
+  final searchPhotoRespo = (ApiResponse<SearchPhotoRespo?>()).obs;
+
+  photoOrder(
+      {int currentPage = 1, int pageSize = ApiConstant.PAGE_SIZE}) async {
+    photoOrderRespo.value = ApiResponse.loading();
+    final data = await _homeRepo.photoOrder();
+    photoOrderRespo.value = data;
+  }
+
+  searchPic(
+      {String query = '',
+      int currentPage = 1,
+      int pageSize = ApiConstant.PAGE_SIZE,
+      bool isFreshCall = true}) async {
+    if (isFreshCall) searchPhotoRespo.value = ApiResponse.loading();
+    final data = await _homeRepo.searchPhoto(
+        query: query, page: currentPage, pageSize: pageSize);
+    searchPhotoRespo.value = data;
+  }
 }
