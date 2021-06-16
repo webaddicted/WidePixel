2f2e46ba8cd4f922f96248cf1ae064bcd859ed69
Deepak Sharma <deepaksharma040695@gmail.com>
2021-06-16 09:10:25 +0530

    Home Api Integrate

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:wallpaper/utils/constant/StrConst.dart';
 
 void main() {
   runApp(MyApp());
+  // runApp(MyApp1());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,6 +18,7 @@ class MyApp extends StatelessWidget {
       title: StrConst.APP_NAME,
       debugShowCheckedModeBanner: false,
       defaultTransition: Transition.rightToLeft,
+
       initialBinding: InitialBinding(),
       theme: ThemeData(
         primarySwatch: Colors.blue,
@@ -27,3 +29,55 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+class MyApp1 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Click Color Card',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Test(),
+    );
+  }
+}
+
+class Test extends StatefulWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  _TestState createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  var test = "hello";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(test),
+          backgroundColor: Color.fromRGBO(58, 133, 191, 1),
+        ),
+        body: Column(
+            children: [
+            Row(children: [
+            Text(test),
+        IconButton(
+          icon: Icon(Icons.delete_forever),
+          color: Colors.red,
+          onPressed: () {
+            setState(() {
+              test = "notworking:(";
+
+            });
+          },
+        ),
+        ]),]
+    ));
+  }
+}
+
+
+
