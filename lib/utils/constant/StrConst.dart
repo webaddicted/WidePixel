902a31cb27e75b877c893aa9737c87c007a521d2
Deepak Sharma <deepaksharma040695@gmail.com>
2021-06-16 09:09:56 +0530

    Home Api Integrate

--- lib/utils/constant/StrConst.dart
+++ lib/utils/constant/StrConst.dart
@@ -1,12 +1,16 @@
 class StrConst {
   static const String APP_NAME = 'Wide Pixel';
   static bool DEBUG = false;
+
 //  {START TITLE}
-  static var DUMMY_TXT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmood tempor inciddidunt ut labore et dolore magna sed do sed do aliqua.';
+  static var DUMMY_TXT =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmood tempor inciddidunt ut labore et dolore magna sed do sed do aliqua.';
   static String NO_DATA_FOUND = 'No Data Found';
-  static var SOMETHING_WENT_WRONG = 'Something went wrong please try again later.';
-  static var INVITE_TITLE = APP_NAME+"\nRefer and Earn";
-  static var REFER_DESC = 'Invite and earn 50 point\n\nhttps://play.google.com/store/apps/details?id=com.webaddicted.movies4u';
+  static var SOMETHING_WENT_WRONG =
+      'Something went wrong please try again later.';
+  static var INVITE_TITLE = APP_NAME + "\nRefer and Earn";
+  static var REFER_DESC =
+      'Invite and earn 50 point\n\nhttps://play.google.com/store/apps/details?id=com.webaddicted.movies4u';
   static var are_you_sure_exit = 'Are you sure you want to exit this app?';
   static var yes = 'Yes';
   static var no = 'No';
@@ -14,10 +18,18 @@ class StrConst {
 
   static var choose_topic = 'Choose Topics';
 
-  static var choose_topic_desc = 'you can choose topics and we gave suggest suitable products for you';
+  static var choose_topic_desc =
+      'you can choose topics and we gave suggest suitable products for you';
 
   static var skip = 'Skip';
 
   static String? NO_INTERNET_CONNECTION = 'No Internet Connection';
 
+  static var TITLE_NATURE = 'Nature';
+
+  static var TITLE_GIRLS = 'Girls';
+  static var TITLE_CAR = 'Car';
+  static var TITLE_ROBOTIC = 'Robot';
+  static var TITLE_GIRLS_DRESS = 'Girls Dress';
+  static var TITLE_TECHNOLOGY = 'Technology';
 }
