6ed333f54ce806c5b61a135ca5c3dd8753d3be75
Deepak Sharma <deepaksharma040695@gmail.com>
2021-06-16 13:06:30 +0530

    Home Api Integrate

--- lib/utils/constant/DummyData.dart
+++ lib/utils/constant/DummyData.dart
@@ -1,5 +1,9 @@
 import 'package:wallpaper/data/bean/CategoryBean.dart';
 
+
+
+
+
 List<CategoryBean>  categoryBean() =>[
 CategoryBean('Abstract', '',
 'https://img.freepik.com/free-vector/gradient-liquid-abstract-background_52683-60469.jpg?size=626&ext=jpg'),
@@ -28,14 +32,22 @@ CategoryBean('Random', '', 'https://cdn.wallpapersafari.com/17/16/1lefPD.jpg'),
       'https://c4.wallpaperflare.com/wallpaper/636/353/280/spider-man-wallpaper-preview.jpg'),
   CategoryBean('White', '#EDEADE',
       'https://bestwallpapers.in/wp-content/uploads/2018/04/cats-kitten-cute-white-brown-5k-wallpaper-2560x1600.jpg'),
-  CategoryBean('Red', '#FF0000',
+    CategoryBean(
+        'Yellow', '#FFFF00', 'https://wallpapercave.com/wp/wp8106092.jpg'),
+    CategoryBean(
+        'Orange', '#FFA500', 'https://miro.medium.com/max/4000/0*r-fg6tCOveGFWFsG.jpg'),
+    CategoryBean('Red', '#FF0000',
       'https://images.hdqwalls.com/wallpapers/red-mask-neon-eyes-4k-0k.jpg'),
+    CategoryBean('Purple', '#800080',
+        'https://i.pinimg.com/originals/06/03/09/060309f2d6d87160af54dc9237376c3b.jpg'),
+    CategoryBean('Magenta', '#FF00FF',
+        'https://content.latest-hairstyles.com/wp-content/uploads/dark-purple-and-magenta-hair-color.jpg'),
   CategoryBean('Green', '#00FF00',
       'https://cdn.statically.io/img/wallpaperaccess.com/full/545792.jpg'),
   CategoryBean('Blue', '#0000FF',
       'https://www.printawallpaper.com/wp-content/uploads/2020/07/Blue_Guitar_on_Brick_Wall_dn.jpg'),
-  CategoryBean(
-      'Yellow', '#FFFF00', 'https://wallpapercave.com/wp/wp8106092.jpg'),
-  CategoryBean('Violet', '#EE82EE',
-      'https://images.alphacoders.com/110/thumb-1920-1103203.png'),
-];
+  CategoryBean('Teal', '#008080',
+      'https://dukerichards.com/wp-content/uploads/2018/07/living-room-interior-design-blue-shade-1024x1024.jpg'),
+    CategoryBean('black_and_white', '#c0c0c0',
+        'https://i.pinimg.com/originals/a6/64/99/a66499d4c8e125576b61031b417605f8.jpg'),
+  ];
