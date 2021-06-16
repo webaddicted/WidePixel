3e4ed787d116bca5aa4d0f12ac37346dc0375b9e
Deepak Sharma <deepaksharma040695@gmail.com>
2021-06-16 09:12:35 +0530

    Home Api Integrate

---
+++ lib/view/home/CategoryTagItem.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:wallpaper/data/bean/CategoryBean.dart';
+import 'package:wallpaper/utils/common/GlobalUtilities.dart';
+import 'package:wallpaper/utils/common/WidgetHelper.dart';
+import 'package:wallpaper/utils/constant/ApiConstant.dart';
+import 'package:wallpaper/utils/constant/ColorConst.dart';
+import 'package:wallpaper/utils/constant/DummyData.dart';
+import 'package:wallpaper/utils/constant/RoutersConst.dart';
+
+/// Author : Deepak Sharma(Webaddicted)
+/// Email : deepaksharmatheboss@gmail.com
+/// Profile : https://github.com/webaddicted
+
+class CategoryTagItem extends StatelessWidget {
+  String apiName;
+  String title;
+
+  CategoryTagItem({this.title = '', this.apiName = ''});
+  var data = categoryBean();
+  @override
+  Widget build(BuildContext context) {
+    double height = 100;
+    double width = 100;
+    if (apiName == ApiConstant.COLOR_IMAGE) {
+      height = 100;
+      width = 100;
+      data = categoryBean();
+    } else if (apiName == ApiConstant.Circle_Color_50) {
+      height = 50;
+      width = 50;
+      data = colorCategoryBean();
+    }
+    return categoryTag();
+  }
+  Widget categoryTag() {
+    return Container(
+        margin: EdgeInsets.only(left: 5),
+        child: getList(
+            height: 60,
+            itemCount: categoryBean().length,
+            widget: (context, index) {
+              CategoryBean item = categoryBean()[index];
+              return InkWell(
+                onTap: () => Get.toNamed(RoutersConst.list),
+                child: Container(
+                  padding: EdgeInsets.only(top: 5, bottom: 5),
+                  child: Card(
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Center(
+                      child: Container(
+                          padding: EdgeInsets.only(left: 20, right: 20),
+                          child: getTxtBlackColor(msg: '${item.name}')),
+                    ),
+                  ),
+                ),
+              );
+            }));
+  }
+}
